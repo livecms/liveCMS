533e67aa054d2c911b9726b9fc95e754553a6b64
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-01-17 22:55:27 +0700

    edit db congif

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', env('OPENSHIFT_MYSQL_DB_HOST', 'localhost')),
+            'host'      => env('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')),
             'database'  => env('DB_DATABASE', 'livecms'),
-            'username'  => env('DB_USERNAME', env('OPENSHIFT_MYSQL_DB_USERNAME', 'forge')),
-            'password'  => env('DB_PASSWORD', env('OPENSHIFT_MYSQL_DB_PASSWORD', 'forge')),
+            'username'  => env('DB_USERNAME', getenv('OPENSHIFT_MYSQL_DB_USERNAME')),
+            'password'  => env('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD')),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
