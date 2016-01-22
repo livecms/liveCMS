3d9de769a0f18b395e4c6865f3f3e819bed38a87
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-01-22 10:56:23 +0700

    add validation rules to kategori

--- app/Kategori.php
+++ app/Kategori.php
@@ -5,4 +5,14 @@ namespace App;
 class Kategori extends BaseModel
 {
     protected $fillable = ['kategori', 'slug'];
+
+    public function rules()
+    {
+    	if (!request()->has('slug')) request()->merge(['slug' => str_slug(request()->get('kategori'))]);
+
+    	return [
+    		'kategori' => 'required|unique:kategoris,kategori'.(($this->id != null) ? ','.$this->id : ''),
+    		'slug' => 'required|unique:kategoris,slug'.(($this->id != null) ? ','.$this->id : ''),
+    	];
+    }
 }
