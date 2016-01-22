e3ae04f2707e1604cd66646776284cecd7858d09
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-01-22 10:52:37 +0700

    add validation rules to tag

--- app/Tag.php
+++ app/Tag.php
@@ -5,4 +5,14 @@ namespace App;
 class Tag extends BaseModel
 {
     protected $fillable = ['tag', 'slug'];
+
+    public function rules()
+    {
+    	if (!request()->has('slug')) request()->merge(['slug' => str_slug(request()->get('tag'))]);
+
+    	return [
+    		'tag' => 'required|unique:tags,tag'.(($this->id != null) ? ','.$this->id : ''),
+    		'slug' => 'required|unique:tags,slug'.(($this->id != null) ? ','.$this->id : ''),
+    	];
+    }
 }
