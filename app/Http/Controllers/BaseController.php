2da6883a19126fc613eebd368953d6f7ebeb66bb
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-02-04 02:29:20 +0700

    refactoring how to show global_params in controllers

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Cache;
-use View;
-use App\Setting as Param;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -16,10 +13,6 @@ class BaseController extends Controller
 
     public function __construct()
     {
-        $global_params = Cache::rememberForever('global_params', function() {
-            return Param::lists('value', 'key');
-        });
-
-        $this->global_params = $global_params;
+        $this->global_params = globalParams();
     }
 }
