3622ce616a5cb95e58ab94392d62e100e23f4823
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-02-04 08:28:00 +0700

    edit travis.yml and testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://127.0.0.1';
+    protected $baseUrl = 'http://localhost:8000';
 
     /**
      * Creates the application.
