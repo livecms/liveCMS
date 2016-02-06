c1fa334b7de4a75e134015d5487ce2764ea09798
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-02-06 18:03:56 +0700

    add user controller

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\BackendController;
+use App\User as Model;
 
 class UserController extends BackendController
 {
-    //	
+    public function __construct(Model $model, $base = 'user')
+    {
+        parent::__construct($model, $base);
+        view()->share('breadcrumb2Icon', 'users');
+        view()->share('fields', array_except($this->model->getFields(), ['id']));
+    }
 }
