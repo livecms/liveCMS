c2b7085d3cb6261b5b0728616ea771fd89db2b49
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-02-13 01:49:44 +0700

    make seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(SetupSeeder::class);
         $this->call(DefaultRoleSeeder::class);
         $this->call(DefaultSuperAdminUserSeeder::class);
         $this->call(DefaultAdminUserSeeder::class);
