80454f70dc51c83ca1ca99e9a1adfbe50309da0c
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-02-13 18:59:29 +0700

    change artikel width

--- resources/views/admin/artikel/form.blade.php
+++ resources/views/admin/artikel/form.blade.php
@@ -4,46 +4,46 @@
 
 	@include('partials.error')
 	<div class="row form-group">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			{!! Form::label('judul', 'Judul', ['class' => 'control-label']) !!}
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-10">
 			{!! Form::text('judul', $artikel->judul, ['class' => 'form-control']) !!}
 		</div>
 	</div>
 
 	<div class="row form-group">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			{!! Form::label('slug', 'Slug', ['class' => 'control-label']) !!}
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-10">
 			{!! Form::text('slug', $artikel->slug, ['class' => 'form-control']) !!}
 		</div>
 	</div>
 
 	<div class="row form-group">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			{!! Form::label('isi', 'Isi', ['class' => 'control-label']) !!}
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-10">
 			{!! Form::textarea('isi', $artikel->isi, ['class' => 'form-control']) !!}
 		</div>
 	</div>
 
 	<div class="row form-group">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			{!! Form::label('kategori', 'Kategori', ['class' => 'control-label']) !!}
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-10">
 			{!! Form::select('kategoris[]', $kategoris, $artikel->kategoris->lists('id')->all(), ['class' => 'form-control', 'multiple' => true]) !!}
 		</div>
 	</div>
 
 	<div class="row form-group">
-		<div class="col-md-3">
+		<div class="col-md-2">
 			{!! Form::label('tag', 'Tag', ['class' => 'control-label']) !!}
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-10">
 			{!! Form::select('tags[]', $tags, $artikel->tags->lists('id')->all(), ['class' => 'form-control', 'multiple' => true]) !!}
 		</div>
 	</div>
@@ -51,5 +51,5 @@
 @stop
 
 @section('content')
-@include('partials.form', ['width' => '10'])
+@include('partials.form', ['width' => '12'])
 @stop
