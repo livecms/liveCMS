a1927f479eec9ac49cf74076c8e46709c960b288
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-04-16 09:55:44 +0700

    add Super Model Trait

---
+++ app/liveCMS/Models/Traits/SuperModelTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\liveCMS\Models\Traits;
+
+trait SuperModelTrait
+{
+    public function allowsUserAccess($user)
+    {
+        return true;
+    }
+
+    public function allowsUserRead($user)
+    {
+        return $user->is_super;
+    }
+
+    public function allowsUserCreate($user)
+    {
+        return $user->is_super;
+    }
+
+    public function allowsUserUpdate($user)
+    {
+        return $user->is_super;
+    }
+
+    public function allowsUserDelete($user)
+    {
+        return $this->is_super;
+    }
+}
