40ab31ce3d40c1853650dddf6a1b4ef5f699f7bf
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-04-23 05:55:39 +0700

    add article and update themes

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -8,18 +8,40 @@ use App\Models\Article as Model;
 
 class ArticleController extends UserController
 {
-    public function __construct(Model $model, $base = 'article')
+    public function __construct(Model $model, $base = 'myarticle')
     {
         $this->breadcrumb2Icon  = 'pencil';
         parent::__construct($model, $base);
+        $this->fields = ['title', 'permalink', 'picture', 'published at'];
         // $this->model = $this->model->setAllSites(false);
     }
 
     public function index(Request $request)
     {
-        parent::index($request);
-
-        return view('user.article.home', compact(camel_case($this->base)));
+        $this->title        = title_case(trans('livecms.myarticle'));
+        $this->description  = trans('backend.alllist', ['list' => title_case(trans('livecms.'.$this->base))]);
+        $this->breadcrumb2  = trans('livecms.myarticle');
+        $this->breadcrumb3  = trans('backend.seeall');
+        $this->view->share();
+        return view('user.myarticle.home', compact(camel_case($this->base)));
     }
+
+    public function create(Request $request)
+    {
+        $model = $this->model;
+        ${camel_case($this->base)} = $model;
+
+        $this->title        = trans('backend.add');
+        $this->description  = trans('backend.adding', ['data' => trans('livecms.'.$this->base)]);
+        $this->breadcrumb3  = trans('backend.add');
+        $this->action       = 'store';
+        $this->params       = array_merge($request->query() ? $request->query() : []);
+
+        $this->view->share();
+
+        $this->loadFormClasses($model);
+
+        return view("user.".camel_case($this->base).".form", compact(camel_case($this->base)));
+    }    
 
 }
---
+++ resources/views/user/myarticle/home.blade.php
@@ -0,0 +1,50 @@
+@extends('user')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8">
+    @if(isset($withoutAddButton))
+    @else
+        <p>
+        <a href="{{ action($baseClass.'@create', request()->query()) }}" class="btn btn-danger">Tambah</a> &nbsp;<span>Klik untuk menambah data {{ trans('livecms.'.$base) }}.</span>
+        </p>
+    @endif
+    </div>
+    <div class="col-md-4 row">
+        <div class="col-xs-12 visible-xs visible-sm">&nbsp;</div> 
+        @yield('index.submenu')
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <table class="table datatables display responsive no-wrap">
+            <thead>
+            @foreach(array_values($fields) as $field)
+                <th>{{ trans('livecms.'.strtolower($field)) }}</th>
+            @endforeach
+                <th>Menu</th>
+            </thead>
+        </table>        
+    </div>
+</div>
+@stop
+
+@section('css.header')
+<style type="text/css">
+    
+.dataTables_length {
+    display: none;
+}
+
+.dataTables_info {
+    display: none;
+}
+
+.dataTables_paginate {
+}
+
+.sticky-wrap {
+    overflow-x: hidden;
+}
+</style>
+@stop
