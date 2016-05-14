96302c1fc84686d270585b16c69a68cab5da8a11
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-05-15 03:49:26 +0700

    fix including helpers file in template

--- app/liveCMS/Controllers/FrontendController.php
+++ app/liveCMS/Controllers/FrontendController.php
@@ -8,7 +8,7 @@ class FrontendController extends BaseController
 {
     public function __construct()
     {
-        $helpers = config('view.paths.0').str_replace('.', DIRECTORY_SEPARATOR, theme('front', 'partials.helpers'));
+        $helpers = config('view.paths.0').DIRECTORY_SEPARATOR.str_replace('.', DIRECTORY_SEPARATOR, theme('front', 'partials.helpers')).'.php';
         $variables = file_exists($helpers) ? require_once $helpers : [];
         view()->share($variables);
     }
